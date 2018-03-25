95add0c344ceb4fb00d48c9e377036659888d71d
Jimmy Arts <arts.jimmy@gmail.com>
2018-03-25 17:06:55 +0200

    Test commit

--- deltatick/Source/Extensions/UserDefaults+Extension.swift
+++ deltatick/Source/Extensions/UserDefaults+Extension.swift
@@ -1,11 +1,3 @@
-//
-//  UserDefaults+Extension.swift
-//  deltatick
-//
-//  Created by Jimmy Arts on 22/03/2018.
-//  Copyright © 2018 Jimmy. All rights reserved.
-//
-
 import Foundation
 
 extension UserDefaults {
